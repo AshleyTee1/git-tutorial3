ff9169f15153a9d56730ce747eec1c6b2032f172
Ashley <6611190009@mut.ac.th>
2025-07-14 13:52:31 +0700

    commit4

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
 <?php
     echo "This is a git tutorial";
     echo " lola";
+    echo " lola";
+    echo " Tel :";
+    echo " lola";
+    echo " lola";
+
+    echo " Tel :";
+    echo " us";
+    echo " email";
+?>
 ?>
--- menu.php
+++ menu.php
@@ -2,5 +2,10 @@
     echo " HOme";
     echo "product";
     echo " me";
+    echo " lola";
+    echo " lola";
+    echo " Tel :";
+    echo " lola";
+    echo " lola";
 
 ?>
